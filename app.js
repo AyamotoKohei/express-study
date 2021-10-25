77383297579dad98d6a60435e1dbadc850c5a8a7
AyamotoKohei <koh14taka2@gmail.com>
2021-10-25 23:15:03 +0900

    add: express学習用の変更

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+'use strict';
+
+// debugモジュールの読込
+const debug = require('debug');
+
+// 1秒ごとにログを出力する
+const debugInfo = debug('module:info');
+setInterval(() => {
+  debugInfo('some information.');
+}, 1000);
+const debugError = debug('module:error');
+setInterval(() => {
+  debugError('some error.');
+}, 1000);
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
